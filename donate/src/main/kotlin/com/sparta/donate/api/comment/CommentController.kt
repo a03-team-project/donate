ca8f7d926552fd743c10ff9380c8f827909b5d6d
Bang <threejun1007@gmail.com>
2024-01-22 20:57:11 +0900

    댓글 CRUD api 작성완료

--- donate/src/main/kotlin/com/sparta/donate/api/comment/CommentController.kt
+++ donate/src/main/kotlin/com/sparta/donate/api/comment/CommentController.kt
@@ -1,7 +1,51 @@
 package com.sparta.donate.api.comment
 
+import com.sparta.donate.application.comment.CommentService
+import com.sparta.donate.dto.comment.request.CommentRequest
+import com.sparta.donate.dto.comment.response.CommentResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class CommentController {
+@RequestMapping("/api/v1/comments")
+class CommentController(
+    private val commentService: CommentService
+) {
+    @PostMapping
+    fun creatComment(@RequestBody commentRequest: CommentRequest): ResponseEntity<CommentResponse> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(commentService.createComment(commentRequest))
+    }
+
+    @PatchMapping("/{commentId}")
+    fun updateComment(
+        @PathVariable commentId: Long,
+        @RequestBody commentRequest: CommentRequest
+    ): ResponseEntity<CommentResponse>{
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(commentService.updateComment(commentRequest))
+    }
+
+
+    @DeleteMapping("/{commentId}")
+    fun deleteComment(
+        @PathVariable commentId: Long,
+        @RequestBody commentRequest: CommentRequest
+    ): ResponseEntity<Unit> {
+        commentService.deleteComment(commentId)
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
+    }
+
+
 }
--- donate/src/main/kotlin/com/sparta/donate/application/comment/CommentService.kt
+++ donate/src/main/kotlin/com/sparta/donate/application/comment/CommentService.kt
@@ -1,7 +1,21 @@
 package com.sparta.donate.application.comment
 
+import com.sparta.donate.domain.comment.Comment
+import com.sparta.donate.dto.comment.request.CommentRequest
+import com.sparta.donate.dto.comment.response.CommentResponse
+import com.sparta.donate.repository.member.MemberRepository
 import org.springframework.stereotype.Service
 
+
+
 @Service
-class CommentService {
+class CommentService(
+    private val memberRepository: MemberRepository
+) {
+
+    fun createComment(request: CommentRequest): CommentResponse {
+
+        Comment.toEntity(request, member, post)
+
+    }
 }
